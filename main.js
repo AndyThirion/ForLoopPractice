1078697f067803afdf52db0b232ab555a2fec44f
AndyThirion <andy.thirion@gmail.com>
2014-09-16 11:05:16 -0600

    First commit/complete

---
+++ main.js
@@ -0,0 +1,59 @@
+// Problem 1
+
+console.log("Problem 1: \n");
+
+for (i=0; i <= 500; i+= 100){
+	console.log(i);
+}
+
+// Problem 2
+
+console.log("\nProblem 2: \n");
+
+for (i = 1; i <= 64; i *= 2) {
+	console.log(i);
+}
+
+// Problem 3
+
+console.log("\nProblem 3: \n");
+
+for (i= 1; i < 4; i++) {
+	for (counter = 1; counter <= 3; counter++) {
+		console.log(i);
+	}
+}
+
+// Problem 4
+
+console.log("\nProblem 4: \n");
+
+for (i=0; i<11; i+=2) {
+	console.log(i);
+}
+
+// Problem 5
+
+console.log("\nProblem 5: \n");
+
+for (i=3; i<=15; i+=3) {
+	console.log(i);
+}
+
+// Problem 6
+
+console.log("\nProblem 6: \n");
+
+for (i=9; i>=0; i--){
+	console.log(i);
+}
+
+// Problem 7
+
+console.log("\nProblem 7: \n");
+
+for (count = 1; count <=3; count++) {
+	for (i=0; i<=3; i++) {
+		console.log(i)
+	}
+}
